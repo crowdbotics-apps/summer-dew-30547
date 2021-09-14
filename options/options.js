d01c4c5db34afa44febf80374c8e97d3cb300f60
Crowdbotics <team@crowdbotics.com>
2021-09-14 21:46:35 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "summer_dew_30547Identifier",
+  url: "https://summer_dew_30547Identifier.botics.co",
+  api: "https://summer_dew_30547Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
